e955944af8e2f77603102ce332885c12d05a2089
TolaAbiodun <adetolaabiodun04@gmail.com>
2021-07-10 02:22:24 +0100

    changed import

--- pyavo/avotools/__init__.py
+++ pyavo/avotools/__init__.py
@@ -10,5 +10,6 @@ from .impedance import ai, ei, norm_ei, lame
 
 from .log_crossplot import crossplot, plot_imp, avo_plot
 
-from .moduli import youngs, bulk, poisson, mu, lame_param, p_mod, comp_vel, shear_vel, elastic_mod
+from .moduli import youngs, bulk, poisson, comp_vel, shear_vel, elastic_mod
 
+from .moduli import mu, lame_param, p_mod
