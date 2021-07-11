ad9873224d39711d049c3643db8de9338f919dd8
TolaAbiodun <adetolaabiodun04@gmail.com>
2021-07-11 01:11:54 +0100

    edit init config

--- pyavo/__init__.py
+++ pyavo/__init__.py
@@ -2,9 +2,12 @@
 Initialize the library.
 :license: MIT
 """
-from . import avotools
-from . import seismodel
+from pyavo import avotools
+from pyavo import seismodel
+from .avotools import impedance, gassmann_substitution, log_crossplot, moduli
+from .seismodel import angle_stack, tuning_wedge, tuning_prestack, RPTM, wavelet
 
+from .pyavo import PyAvoError
 __version__ = "unknown"
 try:
     from ._version import __version__
