4e96fa380679236688f3ea9a4d83b3d1ab66d96e
TolaAbiodun <adetolaabiodun04@gmail.com>
2021-07-15 01:32:05 +0100

    editer version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Tola Abiodun'
 author = 'Tola Abiodun'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
